360826aed0f7e19921bf25aac05191a64dae01e2
George S <141447199+neogeojr@users.noreply.github.com>
2026-03-17 05:48:45 -0700

    Added PostAuthor, ReactionsButtons, TimeAgo and UserSlice

--- src/features/post/AddPostForm.jsx
+++ src/features/post/AddPostForm.jsx
@@ -1,23 +1,37 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postAdded } from "./postSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const users = useSelector(selectAllUsers);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
 
   const onSavedPostClicked = () => {
     if (title && content) {
-      (dispatch(postAdded(title, content)), setTitle(""));
+      dispatch(postAdded(title, content, userId));
+      setTitle("");
       setContent("");
+      setUserId("");
     }
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const userOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
@@ -31,6 +45,12 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          {userOptions}
+        </select>
+
         <label htmlFor="postContent">Content:</label>
         <input
           type="text"
@@ -39,7 +59,7 @@ const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavedPostClicked}>
+        <button type="button" onClick={onSavedPostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
--- src/features/post/PostList.jsx
+++ src/features/post/PostList.jsx
@@ -1,13 +1,25 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionsButtons from "./ReactionsButtons";
 
 const PostList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionsButtons post={post} />
     </article>
   ));
 
